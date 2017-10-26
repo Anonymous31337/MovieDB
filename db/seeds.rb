93f270fae66f82e82ae7ef1b00b82c78508671bf
Anonymous31337 <kevinwrede@gmx.net>
2017-10-26 15:59:17 +0200

    Seed Data added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Movie.destroy_all
+avatar = Movie.create(title: "Avatar", originaltitle: "Avatar", releaseyear: 2004, cover:"", length: 126, ageres:12, synopsis:"Dies ist eine Kurzbeschreibung")
+
+Actor.destroy_all
+actor1 = Actor.create(name: "Cameron", fname: "James", birthdate:1932)
+avatar.actors = [actor1]
