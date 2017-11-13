d55e92316a42430e5583bbc21315f99f59b298e4
Franziska Pätzold <s0554620@htw-berlin.de>
2017-11-13 15:57:42 +0100

    tests s001-s004

--- spec/feature/test_s001_2.rb
+++ spec/feature/test_s001_2.rb
@@ -2,12 +2,14 @@ require 'capybara/rails'
 require 'rails_helper'
 
 describe 's001/2: user can see a list of movies and their actors' do
-  before :each do
-    Movie.create(title: "Der König der Löwen", originaltitle: "The Lion King", releaseyear: 1994, cover: "url", legnth: 88, ageres: 0, synopsis: "bla mit Löwen")
-  end
 
   it 'shows the movies' do
     visit root_path
-    expect(page).to have_content("Der König der Löwen")
+    # following movies should already exist from seeds
+    expect(page).to have_content("Avatar")
+    expect(page).to have_content("Der große Gatsby")
+    expect(page).to have_content("The Wolf of Wallstreet")
+    expect(page).to have_content("Shutter Island")
+    expect(page).to have_content("Fight Club")
   end
 end
