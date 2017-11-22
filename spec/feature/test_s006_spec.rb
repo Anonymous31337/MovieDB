e6af49393267c96fda66aa752fe6df0267d42788
Jack <jack.la@icloud.com>
2017-11-22 21:43:17 +0100

    add test for user story s006 (test is not tested yet)

---
+++ spec/feature/test_s006_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+require 'spec_helper'
+describe 's006 As a privileged user, I can add new genres.' do
+  before :each do
+        user = User.create(email: 'someone@htw-berlin.de', password: 'geheimgeheim', password_confirmation: 'geheimgeheim')
+        login_as(user, :scope => :user)
+    Genre.create(name: "Thriller")
+  end
+
+  it 'add genre' do
+    visit "/genre"
+
+    # note that these are all ids rather than texts.
+    fill_in 'genre_id', with: 'Thriller'
+
+    click_button 'Create Genre'
+
+    expect(page).to have_content('Genre: Thriller')
+    end
