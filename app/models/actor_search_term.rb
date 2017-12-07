35be8658cd576ef4534e427b48f702c8c2086816
Franziska Pätzold <s0554620@htw-berlin.de>
2017-12-07 16:04:22 +0100

    s007: added search for actors (works)

---
+++ app/models/actor_search_term.rb
@@ -0,0 +1,25 @@
+class ActorSearchTerm
+  attr_reader :where_clause, :where_args, :order
+  def initialize(search_term)
+    search_term = search_term.downcase
+    @where_clause = ""
+    @where_args = {}
+    build_for_lname_search(search_term)
+  end
+
+  def build_for_lname_search(search_term)
+    @where_clause << case_insensitive_search( :name)
+    @where_args[:name] = starts_with(search_term)
+    @where_clause << " OR #{case_insensitive_search(:fname)}"
+    @where_args[:fname] = starts_with(search_term)
+    @order = "name asc"
+  end
+
+  def starts_with(search_term)
+    search_term + "%"
+  end
+
+  def case_insensitive_search(field_name)
+    "lower(#{field_name}) like :#{field_name}"
+  end
+end
