ac1bc650bfb7e98a0e3d0338b1f928cbede939dd
Franziska Pätzold <s0554620@htw-berlin.de>
2017-12-20 15:10:52 +0100

    S008-S013 rspec tests update

---
+++ spec/feature/s007_search_spec.rb
@@ -0,0 +1,8 @@
+#s007: search
+
+require 'rails_helper'
+describe 's007: As a user, I can search for movies by name, so that I can choose one of them.' do
+  before :each do
+
+  end
+end
---
+++ spec/feature/s008_gan_spec.rb
@@ -0,0 +1,1 @@
+#S008: use website via internet
